12c580ebf52ea5e9a3bd0c792d73140b93fecdb5
HanSaem Lee <dlgkstoa012@naver.com>
2021-08-30 21:45:34 +0900

    0830

--- 06.MySQL/db_module.py
+++ 06.MySQL/db_module.py
@@ -5,14 +5,13 @@ def get_monthly_data(config):
     conn = pymysql.connect(**config)
     cur = conn.cursor()
 
-    sql = '''
-        SELECT DATE_FORMAT(sdate, '%m') AS 'month',
+    sql = """
+        SELECT DATE_FORMAT(sdate, '%m') AS `month`, 
             SUM(revenue) AS revenue, SUM(profit) AS profit
             FROM sales_book
-            GROUP BY 'month'
-            ORDER BY 'month';
-    '''
-
+            GROUP BY `month`
+            ORDER BY `month`;
+    """
     cur.execute(sql)
     results = cur.fetchall()
 
@@ -25,12 +24,12 @@ def get_data_by_company(config):
     conn = pymysql.connect(**config)
     cur = conn.cursor()
 
-    sql = '''
+    sql = """
         SELECT scompany, SUM(revenue) AS revenue, SUM(profit) AS profit
             FROM sales_book
             GROUP BY scompany
             ORDER BY revenue desc;
-    '''
+    """
     cur.execute(sql)
     results = cur.fetchall()
 
@@ -39,16 +38,18 @@ def get_data_by_company(config):
     return results
 
 # 3. 거래처별 판매상품 및 수량
-def  get_products_by_company(config):
+def get_products_by_company(config):
     conn = pymysql.connect(**config)
     cur = conn.cursor()
 
-    sql = '''
-        SELECT scompany, pname, SUM(sunit) AS unit
+    sql = """
+        SELECT scompany, pname, SUM(sunit) AS sunit
             FROM sales_book
             GROUP BY scompany, pname
             ORDER BY scompany;
-    '''
+    """
+    cur.execute(sql)
+    results = cur.fetchall()
 
     cur.close()
     conn.close()
@@ -59,11 +60,33 @@ def get_data_by_products(config):
     conn = pymysql.connect(**config)
     cur = conn.cursor()
 
-    sql = '''
+    sql = """
         SELECT pname, SUM(sunit) AS unit,
-                        SUM(revenue) AS revenue, SUM(profit) AS profit
+		        SUM(revenue) AS revenue, SUM(profit) AS profit
             FROM sales_book
             GROUP BY pid
-            ORDER BY pid
-    '''
-    cur.
+            ORDER BY pid;
+    """
+    cur.execute(sql)
+    results = cur.fetchall()
+
+    cur.close()
+    conn.close()
+    return results
+
+# 5. 카테고리별 매출/이익
+def get_data_by_category(config):
+    conn = pymysql.connect(**config)
+    cur = conn.cursor()
+
+    sql = """
+        SELECT pcategory, SUM(revenue) AS revenue, SUM(profit) AS profit
+            FROM sales_book
+            GROUP BY pcategory;
+    """
+    cur.execute(sql)
+    results = cur.fetchall()
+
+    cur.close()
+    conn.close()
+    return results
